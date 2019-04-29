0cb8e02067c25a85667536c8300b6f6322445ec3
jonsonxu <910829960@qq.com>
2019-04-29 11:50:35 +0800

    use mlab DB

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoUrl: 'mongodb://localhost/project-hub',
+  // mongoUrl: 'mongodb://localhost/project-hub',
+  mongoUrl: 'mongodb://jonson:xzx1998@ds231589.mlab.com:31589/project-hub',
   jwtSecret: 'secret'
 }
