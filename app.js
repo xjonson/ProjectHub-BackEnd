96d55cb72a7657c7ecdf7a8105f59af8a41f9a02
jonsonxu <910829960@qq.com>
2019-04-29 16:11:03 +0800

    update

--- app.js
+++ app.js
@@ -23,6 +23,18 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true }).then(res => {
   console.error(err);
 })
 
+app.all("*", (req, res, next) => {
+  // 设置允许跨域的域名，*代表允许任意域名跨域
+  res.header("Access-Control-Allow-Origin", "*");
+  // 允许的header类型
+  res.header("Access-Control-Allow-Headers", "content-type");
+  // 跨域允许的请求方式
+  res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+  if (req.method.toLowerCase() == 'options')
+    res.send(200); // 让options尝试请求快速结束
+  else
+    next();
+})
 
 // 路由
 const user = require('./app/routes/api/user')
@@ -46,20 +58,6 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/client/index.html', (req, res) => {
   res.sendFile(path.resolve(__dirname + '/dist/client/index.html'));
 });
-
-// 允许跨域
-app.all("*", (req, res, next) => {
-  // 设置允许跨域的域名，*代表允许任意域名跨域
-  res.header("Access-Control-Allow-Origin", "*");
-  // 允许的header类型
-  res.header("Access-Control-Allow-Headers", "content-type");
-  // 跨域允许的请求方式 
-  res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
-  if (req.method.toLowerCase() == 'options')
-    res.send(200); // 让options尝试请求快速结束
-  else
-    next();
-})
 
 // passport 初始化
 app.use(passport.initialize())
